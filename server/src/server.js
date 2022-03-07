acde05d858cbef91bf8f005e142d9f590b9a6912
vishal dogra <dogravishal1598@gmail.com>
2022-03-07 09:23:12 +0530

    fixed bug

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const app = require("./app");
 const { loadPlanetData } = require("./models/planets.model");
 
-const PORT = process.env.PORT | 8000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
