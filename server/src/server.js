2ccaee9b34c1e0315e7e9773174e5251b99ace6f
vishal dogra <dogravishal1598@gmail.com>
2022-03-10 15:21:28 +0530

    added dotenv and axios packages to the app

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 const { loadPlanetData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://nasa-api:XLvNRjXuxNgm5hF@cluster0.osltk.mongodb.net/nasaApp?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
+
   await loadPlanetData();
 
   server.listen(PORT, () => {
