59b37a7a872eb6242e80964a63676ad7a5ddc9c8
vishal dogra <dogravishal1598@gmail.com>
2022-03-10 15:30:19 +0530

    imported dotenv third-party package for env files

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,20 @@
 const http = require("http");
-const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL =
-  "mongodb+srv://nasa-api:XLvNRjXuxNgm5hF@cluster0.osltk.mongodb.net/nasaApp?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
-
+  await mongoConnect();
   await loadPlanetData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
